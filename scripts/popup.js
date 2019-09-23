4fa43867cc9aaa7e3e44028468db95ccce70e936
DjordjeZiga <djordjeziga1994@gmail.com>
2019-09-24 01:34:16 +0200

    version 1.3.0

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,67 @@
-let psmlRadio = document.getElementById('psmlRadio');
-let bplRadio = document.getElementById('bplRadio');
-var storage = chrome.storage.local;
-psmlRadio.onclick = function() {
-    saveSelectedLeague(1);
-};
-bplRadio.onclick = function() {
-    saveSelectedLeague(2);
+let psmlCheck = document.getElementById('psmlCheck');
+let bplCheck = document.getElementById('bplCheck');
+var storage = chrome.storage.sync;
+
+window.onload = function() {
+    getPsmlState();
+    getBplState();
+}
+
+psmlCheck.onclick = function() {
+    if (psmlCheck.checked){
+        savePSMLState(1);
+    } else {
+        savePSMLState(0);
+    }
+    chrome.tabs.query({url: "http://pesdb.net/*"}, function(tab) {
+        chrome.tabs.reload(tab[0].id)
+    });
 };
 
-function saveSelectedLeague(value) {
+bplCheck.onclick = function() {
+    if (bplCheck.checked){
+        saveBPLState(1);
+    } else{
+        saveBPLState(0);
+    }
+    chrome.tabs.query({url: "http://pesdb.net/*"}, function(tab) {
+        chrome.tabs.reload(tab[0].id)
+    });
+};
+
+function getPsmlState() {
+    let psmlState;
+    storage.get("psmlState",function (fetchedData) {
+        psmlState = fetchedData.psmlState;
+        if (psmlState === 1){
+            psmlCheck.checked = true;
+        }
+    });
+}
+
+function getBplState() {
+    let bplState;
+    storage.get("bplState",function (fetchedData) {
+        bplState = fetchedData.bplState;
+        if (bplState === 1){
+            bplCheck.checked = true;
+        }
+    });
+}
+
+
+function savePSMLState(value) {
     var data = {};
-    data['selectedLeague'] = value
-    chrome.storage.sync.set({"selectedLeague": value}, function(){
+    data['psmlState'] = value
+    storage.set({"psmlState": value}, function(){
+        console.log('Value is set to ' + 'dsf');
+    });
+}
+
+function saveBPLState(value) {
+    var data = {};
+    data['bplState'] = value
+    storage.set({"bplState": value}, function(){
         console.log('Value is set to ' + 'dsf');
     });
 }
